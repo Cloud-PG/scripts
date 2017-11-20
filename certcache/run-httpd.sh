02ae662fc7e6ff1bd79acbf051199ac7b169e911
MircoT <MircoT@users.noreply.github.com>
2017-11-20 16:59:39 +0100

    Add first call to get_proxy

--- certcache/run-httpd.sh
+++ certcache/run-httpd.sh
@@ -5,4 +5,5 @@
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
-exec /usr/sbin/apachectl -DFOREGROUND
+exec /usr/bin/python /var/www/cgi-bin/get_proxy \
+     && /usr/sbin/apachectl -DFOREGROUND
