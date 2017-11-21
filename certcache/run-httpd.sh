f5d317cb1af274933ef3241903c879b8e295afa3
MircoT <MircoT@users.noreply.github.com>
2017-11-21 17:44:04 +0100

    Change start of service, start only apache

--- certcache/run-httpd.sh
+++ certcache/run-httpd.sh
@@ -5,5 +5,7 @@
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
-exec /usr/bin/python /var/www/cgi-bin/get_proxy \
-     && /usr/sbin/apachectl -DFOREGROUND
+#exec /usr/bin/python /var/www/cgi-bin/get_proxy \
+#     && /usr/sbin/apachectl -DFOREGROUND
+
+exec /usr/sbin/apachectl -DFOREGROUND
